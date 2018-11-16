37d9afebcae0424d114e0225db48f30ef585a7bb
AlbinoB <albinobraganza21@gmail.com>
2018-11-16 22:57:27 +0530

    finding the shortest path code //final 1.0

--- dijkstras_algorithm.c
+++ dijkstras_algorithm.c
@@ -40,4 +40,73 @@ for( i=0;i<nodes;i++)//display the table
 printf("\n");
 }
 
+
+z=0;
+for( i=0;i<nodes;)
+ {
+ for(j=0 ;j<nodes;j++)
+ {
+   if(a[i][j]!=-1 &&  i!=j && visitedarr[j]!=1)
+   {   sum=b[i]+a[i][j];
+       if(b[j]>sum)
+       {
+           b[j]=sum;
+       }else
+       {
+           if(b[j]==0)
+             b[j]=sum;
+       }
+       c[z++]=j;
+       trueck=1;
+   }
+ }
+   if(trueck==1&&z>1)
+   {
+       arrayindex=c[0];
+       smallest=b[c[0]];
+    for (x=1;x<z;x++)
+   {
+       if(b[c[x]]<=smallest)
+       {
+           smallest=b[c[x]];
+           arrayindex=c[x];
+       }
+   }
+   visitedarr[i]=1;
+   if(i<arrayindex)
+   {i=arrayindex;}else
+   {
+       i=i+1;
+   }
+   trueck=0;
+   z=0;
+   }else
+   {
+       if(trueck==1&&z==1)
+       {
+           arrayindex=c[0];
+           visitedarr[i]=1;
+           i=arrayindex;
+           z=0;
+           trueck=0;
+           }else{
+           if(i==nodes-1)
+              {
+                  i++;
+                  trueck=0;
+              }
+       }
+   }
+   p++;
+   if(p>nodes)
+   {
+     break;
+   }
+ }
+ for( j=0;j<nodes;j++)
+  {
+   printf("\n Minimum distance  %c  to %c is :%d",n[0],n[j],b[j]);
+  }
+  printf("\n");
+
 }
